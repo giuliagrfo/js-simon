99a89908134263c051dedf8782896d6b337dbf1b
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-19 15:12:21 +0200

    generate numbers

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+/*
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+*/
+const numbers = 5;
+// generare 5 numeri casuali 
+
+const numberList = generateNumbers(1, numbers)
+console.log(numberList);
+
+
+function generateNumbers(min, max) {
+    const numberList = []
+    while (numberList.length !== 5) {
+  
+      // genero numero casuale
+      const num = generateRandomNumber(min, max)
+      // verifico se il numero é stato giá inserito nella lista di bombe
+      // se non lo é lo aggiungo altrimenti vado avanti
+      if (!numberList.includes(num)) {
+        // se la condizione é falsa allora eseguo il codice qui dentro
+        numberList.push(num)
+      }
+    }
+    return numberList
+  }
+  
+  
+  
+  function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+ 
